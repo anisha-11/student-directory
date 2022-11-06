a11f82d2c8514cb02fe2e6e6b6012b19e0403fc3
Anisha Hirani <anisha.hirani11@gmail.com>
2022-11-06 22:30:48 +0000

    Taking arguements from ComLine

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
 @students = []
 
 def input_students
+  another = another_student
+  while another 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty, repeat this code 
   while !name.empty? do 
     #add the student hash to the array 
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     #get another name from the user 
-    name = gets.chomp 
+    name = STDIN.gets.chomp 
   end 
 end
 
@@ -44,6 +46,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else 
@@ -61,13 +65,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first arguement from the command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exist?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else 
+    puts "Sorry, #{filename} doesn't exist."
+    exit 
+  end
 end
 
 def print_header
@@ -86,3 +102,5 @@ def print_footer
 end
 
 interactive_menu
+load_students
+load_students(list.txt)
