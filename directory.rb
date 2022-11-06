afa79138ad423b287e5148cdb53bceae36f1b895
Anisha Hirani <anisha.hirani11@gmail.com>
2022-11-06 22:38:14 +0000

    Final taking arguements using STDIN

--- directory.rb
+++ directory.rb
@@ -1,42 +1,19 @@
 @students = []
 
-def input_students
-  another = another_student
-  while another 
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #get the first name
-  name = STDIN.gets.chomp
-  #while the name is not empty, repeat this code 
-  while !name.empty? do 
-    #add the student hash to the array 
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-    #get another name from the user 
-    name = STDIN.gets.chomp 
-  end 
-end
-
-def interactive_menu
-  loop do 
-    print_menu
-    process(gets.chomp)
-  end 
-end 
-
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
-end 
-
-def show_students
-  print_header
-  print_student_list
-  print_footer
 end
+
+def interactive_menu
+  loop do 
+    print_menu
+    process(STDIN.gets.chomp)
+  end 
+end  
 
 def process(selection)
   case selection 
@@ -53,6 +30,42 @@ def process(selection)
   else 
     puts "I don't know what you meant, try again"
   end 
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #get the first name
+  name = STDIN.gets.chomp
+  #while the name is not empty, repeat this code 
+  while !name.empty? do 
+    #add the student hash to the array 
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    #get another name from the user 
+    name = STDIN.gets.chomp 
+  end 
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list 
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def save_students
@@ -86,21 +99,5 @@ def try_load_students
   end
 end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print_student_list 
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer
-  puts "Overall, we have #{@students.count} great students"
-end
-
+try_load_students
 interactive_menu
-load_students
-load_students(list.txt)
